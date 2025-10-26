b5bad022805b482d2a1750a4bdd35e605fe77a6e
Maks Stepanenko <stepanenkom156@gmail.com>
2025-10-27 01:12:54 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <windows.h>
+#include "marker.h"
+
+int main() {
+    int arraySize;
+    std::cout << "Enter array size: ";
+    std::cin >> arraySize;
+    std::vector<int> array(arraySize, 0);
+    int threadCount;
+    std::cout << "Enter number of marker threads: ";
+    std::cin >> threadCount;
+    std::vector<HANDLE> threadHandles;
+    std::vector<int> threadParams;
+
+    for (int i = 0; i < threadCount; i++) {
+        threadParams.push_back(i + 1);
+    }
+    return 0;
+}
