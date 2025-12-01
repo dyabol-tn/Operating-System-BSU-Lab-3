088df555e99fb51c2491b4c8a3e3265a2f0323fc
Maks Stepanenko <stepanenkom156@gmail.com>
2025-12-02 00:43:00 +0300

    Add files via upload

--- marker.cpp
+++ marker.cpp
@@ -16,19 +16,22 @@ const int SLEEP_DURATION = 5;
 const int EVENTS_COUNT = 2;
 const int START_EVENT_INDEX = 0;
 const int TERMINATE_EVENT_INDEX = 1;
+const int ZERO_VALUE = 0;
+const int FAILED_WAIT_RESULT = WAIT_FAILED;
+const int WAIT_INFINITE = INFINITE;
 
 DWORD WINAPI marker_thread(LPVOID param) {
     ThreadData* data = (ThreadData*)param;
-    WaitForSingleObject(data->startEvent, INFINITE);
+    WaitForSingleObject(data->startEvent, WAIT_INFINITE);
     srand(data->threadId);
     data->markedCount = 0;
     bool terminated = false;
 
     while (!terminated) {
-        int random_number = rand();
-        int index = random_number % data->arraySize;
+        int randomNumber = rand();
+        int index = randomNumber % data->arraySize;
 
-        if (data->array[index] == 0) {
+        if (data->array[index] == ZERO_VALUE) {
             Sleep(SLEEP_DURATION);
             data->array[index] = data->threadId;
             Sleep(SLEEP_DURATION);
@@ -36,16 +39,26 @@ DWORD WINAPI marker_thread(LPVOID param) {
         }
         else {
             std::cout << "Thread " << data->threadId << " cannot mark element. Marked: " << data->markedCount << ", Failed index: " << index << std::endl;
+
             SetEvent(data->suspendEvent);
-            HANDLE waitEvents[EVENTS_COUNT];
-            waitEvents[START_EVENT_INDEX] = data->startEvent;
-            waitEvents[TERMINATE_EVENT_INDEX] = data->terminateEvent;
+            HANDLE waitEvents[EVENTS_COUNT] = {
+                data->startEvent,
+                data->terminateEvent
+            };
+
             DWORD waitResult = WaitForMultipleObjects(
                 EVENTS_COUNT,
                 waitEvents,
                 FALSE,
-                INFINITE
+                WAIT_INFINITE
             );
+
+            if (waitResult == FAILED_WAIT_RESULT) {
+                std::cerr << "Thread " << data->threadId
+                    << " failed to wait for events." << std::endl;
+                break;
+            }
+
             DWORD signaledEventIndex = waitResult - WAIT_OBJECT_0;
 
             if (signaledEventIndex == TERMINATE_EVENT_INDEX) {
@@ -54,13 +67,10 @@ DWORD WINAPI marker_thread(LPVOID param) {
         }
     }
 
-    const int ZERO_VALUE = 0;
-    for (int i = 0; i < data->arraySize; i++) {
-        if (data->array[i] == data->threadId) {
-            data->array[i] = ZERO_VALUE;
+    for (int elementIndex = 0; elementIndex < data->arraySize; elementIndex++) {
+        if (data->array[elementIndex] == data->threadId) {
+            data->array[elementIndex] = ZERO_VALUE;
         }
     }
-
-    std::cout << "Thread " << data->threadId << " terminated." << std::endl;
     return 0;
 }
